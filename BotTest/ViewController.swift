b1dfdae830a8fc4a5d3f5970d5f0a762cbea8488
Arda Keskiner <akeskiner@ku.edu.tr>
2017-05-03 23:04:26 +0300

    Init

---
+++ BotTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BotTest
+//
+//  Created by Arda Keskiner on 03/05/2017.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
